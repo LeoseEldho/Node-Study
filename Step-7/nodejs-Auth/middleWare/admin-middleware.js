dd801f2ed7da3389854dbe1159f47b9882fe2356
Leose <leoseeldho@gmail.com>
2026-01-29 09:37:56 +0530

    29-01-2026

--- Step-7/nodejs-Auth/middleWare/admin-middleware.js
+++
@@ -1,7 +0,0 @@
-const adminmiddleWare = (req, res, next) => {
-    if (req.userInfo.role !== "Admin") {
-        return res.status(202).json({ message: "admin only allowed" })
-    }
-    next()
-};
-module.exports = adminmiddleWare;
